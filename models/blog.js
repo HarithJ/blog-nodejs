6f063f1e9be432f00115206387390cd94cca2a05
harithj <harithjaved@gmail.com>
2019-11-25 14:29:43 +0300

    - start the app using nodemon - create a model for blog post - create migrations - use sqlite db for development - change the app to use pug templates instead of jade

---
+++ models/blog.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const blog = sequelize.define('blog', {
+    title: DataTypes.STRING,
+    body: DataTypes.TEXT
+  }, {});
+  blog.associate = function(models) {
+    // associations can be defined here
+  };
+  return blog;
+};
