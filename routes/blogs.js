ea952dd407c3c9924565817336da553555c4c0dd
harithj <harithjaved@gmail.com>
2019-11-27 13:21:53 +0300

    add functionality to get all the posts on /blogs route

---
+++ routes/blogs.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+var { blog } = require('../models/index.js');
+
+/* GET all blog posts */
+router.get('/', async function(req, res, next) {
+  try {
+    const posts = await blog.findAll({ raw: true });
+
+    return res.render('posts', { posts });
+  }
+  catch (error) {
+    res.status(500).send(error);
+  }
+});
+
