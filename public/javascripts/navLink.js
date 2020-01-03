c4714010605f4b98c9a48aeb97efccc2f4001945
harithj <harithjaved@gmail.com>
2020-01-03 11:42:38 +0300

    Change navbar's UI
    - change the background color to white and text color to dark
    - add the navbar's content within container-fluid and give margin of 5 
    on both ends
    - instead of sign up link, add a button with 'Get started' text
    - change the text of `login` to `Sign in`
    - after a user logs in, display a hamburger menu on the far right
    - move the 'write' and 'logout' links under the hamburger menu
    - style the hamburger menu according to bootstrap's styles

--- public/javascripts/navLink.js
+++ public/javascripts/navLink.js
@@ -1,20 +1,12 @@
-$( '.navbar-nav a' ).on( 'click', function () {
-	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
-});
-
 $(document).ready(() => {
 	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+
 	if (window.location.pathname === '/') {
 		$('nav a:contains("Read")').parent( 'li' ).addClass( 'active' );
 	}
 
 	else if (window.location.pathname === '/users/login') {
-		$('nav a:contains("Login")').parent( 'li' ).addClass( 'active' );
-	}
-
-	else if (window.location.pathname === '/users/register') {
-		$('nav a:contains("Signup")').parent( 'li' ).addClass( 'active' );
+		$('nav a:contains("Sign in")').parent( 'li' ).addClass( 'active' );
 	}
 
 	$('#signupPassword').password({});
